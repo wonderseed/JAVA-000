32f03a48ca2cc4bf8cdbb67cc9540586c9c57c57
syria <luyiqi_0@qq.com>
2020-10-18 22:57:27 +0800

    自定义ClassLoader加载Hello.xlass

---
+++ Week_01/CustomClassLoader.java
@@ -0,0 +1,45 @@
+package Week_01;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class CustomClassLoader extends ClassLoader {
+    public static void main(String[] args) {
+        try {
+            Class<?> Hello = new CustomClassLoader().findClass("Hello");
+            Method method = Hello.getMethod("hello");
+            Object obj = Hello.newInstance();
+            method.invoke(obj);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        File helloFile = new File(this.getClass().getResource("Hello.xlass").getFile());
+        int length = (int) helloFile.length();
+        byte[] bytes = new byte[length];
+        try {
+            new FileInputStream(helloFile).read(bytes);
+            for (int i = 0; i < length; i++) {
+                bytes[i] =(byte) (255 - bytes[i]);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return super.findClass(name);
+        }
+        return defineClass(name,bytes,0, bytes.length);
+    }
+}
